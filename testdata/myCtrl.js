6fcafa00354fd8d7ff17c62263a8d3be55d505cc
sanjorge11 <sanjorge@live.unc.edu>
2018-12-20 17:34:53 -0500

    edit js

--- testdata/myCtrl.js
+++ testdata/myCtrl.js
@@ -2,9 +2,10 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope, $http) {
     var s = $scope;
+    s.recordArray;
 
     $http.get("records.json").then(function (response) {
-        console.log(response);
+        s.recordArray = response.data.records;
     });
 
 });
